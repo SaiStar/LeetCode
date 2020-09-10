a698fe1a354577eb2987a87d333b83bfa77351c6
SaiStar <2499643808@qq.com>
2020-09-10 09:46:44 +0800

    组合之和（回溯+剪枝）（不重复结果子集，两次剪枝）

--- src/test/java/medium/L40_CombinationSumII.java
+++ src/test/java/medium/L40_CombinationSumII.java
@@ -38,9 +38,9 @@ public class L40_CombinationSumII {
 
     @Test
     public void test(){
-//        int[] candidates = new int[]{10,1,2,7,6,1,5};
-        int[] candidates = new int[]{2,5,2,1,2};
-        System.out.println(combinationSum2(candidates,5));
+        int[] candidates = new int[]{10,1,2,7,6,1,5};
+//        int[] candidates = new int[]{2,5,2,1,2};
+        System.out.println(combinationSum2(candidates,8));
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
@@ -59,15 +59,46 @@ public class L40_CombinationSumII {
             return;
         }
         for (int i=index; i<candidates.length; i++){
-            if (i > index && candidates[i] == candidates[i-1]){
-                continue;
-            }
+            //大剪枝
             if(target-candidates[i] < 0){
                 return;
             }
+            /**
+             * ！！！！！！！！！！重复避免---------看最后解释
+             */
+            //小剪枝，去除同层级的相同元素
+            if (i > index && candidates[i] == candidates[i-1]){
+                continue;
+            }
+
             path.addLast(candidates[i]);
             back(candidates,target-candidates[i],i+1,ans,path);
             path.removeLast();
         }
     }
+    /**
+     * 这个方法最重要的作用是，可以让同一层级，不出现相同的元素。即
+     *                   1
+     *                  / \
+     *                 2   2  这种情况不会发生 但是却允许了不同层级之间的重复即：
+     *                /     \
+     *               5       5
+     *                 例2
+     *                   1
+     *                  /
+     *                 2      这种情况确是允许的
+     *                /
+     *               2
+     * 为何会有这种神奇的效果呢？
+     * 首先 cur-1 == cur 是用于判定当前元素是否和之前元素相同的语句。这个语句就能砍掉例1。
+     * 可是问题来了，如果把所有当前与之前一个元素相同的都砍掉，那么例二的情况也会消失。
+     * 因为当第二个2出现的时候，他就和前一个2相同了。
+     *
+     * 那么如何保留例2呢？
+     * 那么就用cur > begin 来避免这种情况，你发现例1中的两个2是处在同一个层级上的，
+     * 例2的两个2是处在不同层级上的。
+     * 在一个for循环中，所有被遍历到的数都是属于一个层级的。我们要让一个层级中，
+     * 必须出现且只出现一个2，那么就放过第一个出现重复的2，但不放过后面出现的2。
+     * 第一个出现的2的特点就是 cur == begin. 第二个出现的2 特点是cur > begin.
+     */
 }
